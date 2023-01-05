b2ff4bf1d30f69980583554e25b029db334340c9
Cosmin Negureanu <cosmin.negureanu@sap.com>
2023-01-05 13:14:41 +0200

    initial commit

---
+++ BE/src/main/java/com/example/buyit/BuyitApplication.java
@@ -0,0 +1,13 @@
+package com.example.buyit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyitApplication.class, args);
+	}
+
+}
