e7617e0cef9e037b9828c850a942b4fc254017c9
Cosmin - Stefan Negureanu <epsilon.number79@gmail.com>
2023-01-24 22:22:36 +0200

    revamped the technologies used

---
+++ BE/buy-it/src/test/kotlin/com/example/buyit/BuyItApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.buyit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BuyItApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
