8ee9f7e1c20f808796ce11123ace1365e648aba2
Cosmin - Stefan Negureanu <epsilon.number79@gmail.com>
2023-01-26 18:10:55 +0200

    finalized initial version of the NavBar component

--- FE/buy-it/src/homePage/components/NavBar.tsx
+++ FE/buy-it/src/homePage/components/NavBar.tsx
@@ -2,24 +2,69 @@ import MenuIcon from "@mui/icons-material/Menu";
 import { ExpandMore } from "@mui/icons-material";
 import "@fontsource/black-ops-one"
 import SearchIcon from '@mui/icons-material/Search';
+import Brightness4Icon from "@mui/icons-material/Brightness4"
+import QuizIcon from "@mui/icons-material/Quiz"
+import PersonIcon from "@mui/icons-material/Person";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart"
 import "../../index.css"
+
+interface IconType {
+    type: String
+}
 
 function NavBar() {
     return (
-       <div className="bg-blue-900 flex flex-row items-center">
-            <ProductMenu />
-            <h1 style={{fontFamily: "Black Ops One"}} className="text-green-400 text-5xl">
+        <div className="bg-blue-900 flex flex-row items-center justify-between">
+            <Icon type={"menu"}/>
+            <h1 style={{ fontFamily: "Black Ops One" }} className="text-green-400 text-5xl">
                 BuyIT
             </h1>
-       </div>
+            <SearchBar />
+            <Icon type="dark" />
+            <div className="flex flex-row items-center">
+                <Icon type={"quiz"} />
+                <Icon type={"user"} />
+                <Icon type={"cart"} />
+            </div>
+        </div>
     )
 }
 
-function ProductMenu() {
+function Icon(props: IconType) {
+    
+    let icon;
+
+    switch (props.type) {
+        case "menu":
+            icon = <MenuIcon fontSize="medium" className="text-white" />;
+            break;
+        case "dark":
+            icon = <Brightness4Icon fontSize="medium" className="text-white" />
+            break;
+        case "quiz":
+            icon = <QuizIcon fontSize="medium" className="text-white" />
+            break;
+        case "user":
+            icon = <PersonIcon fontSize="medium" className="text-white" />
+            break;
+        case "cart":
+            icon = <ShoppingCartIcon fontSize="medium" className="text-white" />
+            break;
+    }
+
     return (
         <span className="flex flex-row items-end">
-            <MenuIcon fontSize="large" className="text-white"/>
+            {icon}
             <ExpandMore fontSize="small" className="text-white" />
+        </span>
+    )
+}
+
+function SearchBar() {
+    return (
+        <span className="bg-gray-700 flex flex-row items-center h-8">
+            <input placeholder="Search for a product" style={{width: '700px'}} className="bg-gray-700 h-2/3 focus:outline-none m-2 text-white" type="text" />
+            <SearchIcon fontSize="large" className="text-white scale-75" />
         </span>
     )
 }
