8182a12a8f33cc704ee3aff481ecf44e61f060e4
Cosmin - Stefan Negureanu <epsilon.number79@gmail.com>
2023-06-11 16:11:32 +0300

    add styling to all possible QuizQuestionPage renders

--- FE/buy-it/src/QuizQuestionPage.tsx
+++ FE/buy-it/src/QuizQuestionPage.tsx
@@ -1,12 +1,14 @@
-import "./index.css"
+import "./index.css";
 import React, { useState, useEffect } from "react";
-import {useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import "@fontsource/chakra-petch";
+import NavBar from "./components/NavBar";
 
-interface Question  {
+interface Question {
   id: number;
   question: string;
   filterProp: string;
-  answers: Answer[]
+  answers: Answer[];
 }
 
 interface Question_Five {
@@ -21,35 +23,44 @@ interface Answer {
   choice: string;
   answer: string;
   value: any;
-  branch: number
+  branch: number;
 }
 
-function isQuestionFive(question: Question | Question_Five): question is Question_Five {
-  console.log((question as Question_Five).value !== null);
-  debugger;
+function isQuestionFive(
+  question: Question | Question_Five
+): question is Question_Five {
   return (question as Question_Five).value !== null;
 }
 
-function isValidQuestion(question: Question | Question_Five): boolean{
+function isValidQuestion(question: Question | Question_Five): boolean {
   try {
-    console.log(question);
-    if(isQuestionFive(question)) {
-      debugger;
-      return question.id !== undefined && question.question !== undefined && question.filterProp !== undefined && question.value !== undefined && question.branch !== undefined;
+    if (isQuestionFive(question)) {
+      return (
+        question.id !== undefined &&
+        question.question !== undefined &&
+        question.filterProp !== undefined &&
+        question.value !== undefined &&
+        question.branch !== undefined
+      );
     } else {
-      debugger;
-      return question.id !== undefined && question.question !== undefined && question.filterProp !== undefined && question.answers !== undefined;
+      return (
+        question.id !== undefined &&
+        question.question !== undefined &&
+        question.filterProp !== undefined &&
+        question.answers !== undefined
+      );
     }
-  } catch(Error) {
+  } catch (Error) {
     return false;
   }
 }
 
-function QuizQuestion(){
-  const {questionID} = useParams();
-  console.log(`QuestionID is ${questionID}`);
+function QuizQuestion() {
+  const { questionID } = useParams();
   const [filterObject, setFilterObject] = useState({});
-  const [question, setQuestion] = useState<Question | Question_Five>({} as Question | Question_Five);
+  const [question, setQuestion] = useState<Question | Question_Five>(
+    {} as Question | Question_Five
+  );
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
   const [selectedAnswer, setSelectedAnswer] = useState<Answer>({} as Answer);
@@ -59,16 +70,10 @@ function QuizQuestion(){
     setLoading(true);
     fetch(`http://localhost:8080/odata/BuyITService/Questions(${questionID})`)
       .then((response) => {
-        debugger;
-        console.log("Response is: ");
-        console.log(response);
         if (response.ok) return response.json();
         throw response;
       })
       .then((data) => {
-        debugger;
-        console.log("Data is ");
-        console.log(data);
         setQuestion(data);
         setLoading(false);
       })
@@ -82,70 +87,221 @@ function QuizQuestion(){
 
   const handleAnswer = (answer: Answer) => {
     setSelectedAnswer(answer);
-  }
+  };
 
   const handleSubmit = () => {
-    if(selectedAnswer) {
-      setFilterObject(prevState => ({
-          ...prevState,
-          [question.filterProp]: selectedAnswer.value
+    if (selectedAnswer) {
+      setFilterObject((prevState) => ({
+        ...prevState,
+        [question.filterProp]: selectedAnswer.value,
       }));
 
-      if(selectedAnswer.branch !== 0) {
+      if (selectedAnswer.branch !== 0) {
         navigate(`/question/${selectedAnswer.branch}`);
       } else {
         navigate("question/results");
       }
     }
+  };
+
+  if (loading)
+    return (
+      <div
+        style={{
+          fontFamily: "Chakra Petch",
+          height: "100vh",
+          backgroundColor: "#1F1F1F"
+        }}
+      >
+        <NavBar />
+        <div
+          className="flex flex-row justify-center items-center"
+          style={{
+            backgroundColor: "rgba(137, 137, 137, 0.4",
+            height: "90%",
+            backgroundImage: "url(/buyit.jpg)",
+            backgroundSize: "100% 100%",
+            backgroundBlendMode: "multiply",
+          }}
+        >
+          <h1 className="text-white" style={{fontSize: "48px"}}>Loading...</h1>
+        </div>
+      </div>
+    );
+
+  if (error) {
+    let message: string = "Something else went wrong.";
+    console.log(error.message);
+
+    if(error instanceof Response && error.status == 404) {
+      message = `Question ${questionID} not found.`;
+    }
+    if(error.message == "Failed to fetch") {
+      message = "Failed to connect to the server.";
+    }
+
+    return (
+      <div
+        style={{
+          fontFamily: "Chakra Petch",
+          height: "100vh",
+          backgroundColor: "#1F1F1F"
+        }}
+      >
+        <NavBar />
+        <div
+          className="flex flex-row justify-center items-center"
+          style={{
+            backgroundColor: "rgba(137, 137, 137, 0.4",
+            height: "90%",
+            backgroundImage: "url(/buyit.jpg)",
+            backgroundSize: "100% 100%",
+            backgroundBlendMode: "multiply",
+          }}
+        >
+          <h1 className="text-white" style={{fontSize: "48px"}}>Error: {message}</h1>
+        </div>
+      </div>
+    );
   }
-  debugger;
+  
 
-  if (loading) return <p>Loading...</p>;
-
-  if (error) return <p>Something went wrong: {error.message}</p>;
-
-  if(!isValidQuestion(question)) return <p>Question was not received properly</p>
-
+  if (!isValidQuestion(question))
+  return (
+    <div
+      style={{
+        fontFamily: "Chakra Petch",
+        height: "100vh",
+        backgroundColor: "#1F1F1F"
+      }}
+    >
+      <NavBar />
+      <div
+        className="flex flex-row justify-center items-center"
+        style={{
+          backgroundColor: "rgba(137, 137, 137, 0.4",
+          height: "90%",
+          backgroundImage: "url(/buyit.jpg)",
+          backgroundSize: "100% 100%",
+          backgroundBlendMode: "multiply",
+        }}
+      >
+        <h1 className="text-white" style={{fontSize: "48px"}}>Question could not be retrieved properly</h1>
+      </div>
+    </div>
+  );
   else {
-    console.log(question);
-    if(isQuestionFive(question)) {
+    if (isQuestionFive(question)) {
       return (
-        <div className="question-container">
-          <h1>{question.question}</h1>
-          <div className="answers-container">
-            <div className="answer">
-              <input
-                type="text"
-                name="answer"
-                value={question.value}
-                id="inputAnswer"
-              />
-              <label htmlFor={question.value}>{question.value}</label>
+        <div
+          style={{
+            fontFamily: "Chakra Petch",
+            height: "100vh",
+            fontSize: "24px",
+            backgroundColor: "#1F1F1F",
+          }}
+        >
+          <NavBar />
+          <div
+            style={{
+              backgroundColor: "rgba(137, 137, 137, 0.4",
+              height: "90%",
+              backgroundImage: "url(/buyit.jpg)",
+              backgroundSize: "100% 100%",
+              backgroundBlendMode: "multiply",
+            }}
+            className="flex flex-col justify-evenly items-center text-white"
+          >
+            <div
+              className="w-4/5"
+              style={{ backgroundColor: "#2B2828"}}
+            >
+              <h1
+                style={{ backgroundColor: "#001540" }}
+                className="p-4"
+              >
+                {question.id}. {question.question}
+              </h1>
+              <div
+                className="flex flex-col justify-center items-center"
+              >
+                <div className="flex flex-row items-center p-4">
+                  <input
+                    type="text"
+                    name="answer"
+                    value={question.value}
+                    onChange={(event) => console.log(event.target.value)}
+                    id="inputAnswer"
+                    className="bg-black p-3 text-white"
+                  />
+                  <label
+                    className="mx-2"
+                    style={{ fontSize: "40px" }}
+                    htmlFor={question.value}
+                  >
+                    $
+                  </label>
+                </div>
+              </div>
             </div>
+            <button className="p-2 bg-green-700" onClick={handleSubmit}>
+              Submit
+            </button>
           </div>
-          <button onClick={handleSubmit}>Submit</button>
         </div>
       );
     } else {
-      console.log(question);
       return (
-        <div className="question-container">
-          <h1>{question.question}</h1>
-          <div className="answers-container">
-            {question.answers.map((answer) => (
-              <div key={answer.choice} className="answer">
-                <input
-                  type="radio"
-                  name="answer"
-                  value={answer.value}
-                  id={answer.choice}
-                  onChange={() => handleAnswer(answer)}
-                />
-                <label htmlFor={answer.choice}>{answer.answer}</label>
+        <div
+          style={{
+            fontFamily: "Chakra Petch",
+            height: "100vh",
+            fontSize: "24px",
+            backgroundColor: "#1F1F1F",
+          }}
+        >
+          <NavBar />
+          <div
+            style={{
+              fontFamily: "Chakra Petch",
+              backgroundColor: "rgba(137, 137, 137, 0.4)",
+              height: "90%",
+              fontSize: "24px",
+              backgroundImage: "url(/buyit.jpg)",
+              backgroundSize: "100% 100%",
+              backgroundBlendMode: "multiply",
+            }}
+            className="flex flex-col justify-evenly items-center text-white"
+          >
+            <div
+              className="w-4/5"
+              style={{ backgroundColor: "#2B2828" }}
+            >
+              <h1 style={{ backgroundColor: "#001540" }} className="p-4">
+                {question.id}. {question.question}
+              </h1>
+              <div className="flex flex-col justify-between p-3">
+                {question.answers.map((answer) => (
+                  <div key={answer.choice} className="bg-black py-2 px-3 my-2">
+                    <input
+                      type="radio"
+                      name="answer"
+                      value={answer.value}
+                      id={answer.choice}
+                      onChange={() => handleAnswer(answer)}
+                      className="w-4 h-4 text-blue-600 bg-gray-700 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    />
+                    <label className="mx-2" htmlFor={answer.choice}>
+                      {answer.answer}
+                    </label>
+                  </div>
+                ))}
               </div>
-            ))}
+            </div>
+            <button className="p-2 bg-green-700" onClick={handleSubmit}>
+              Submit
+            </button>
           </div>
-          <button onClick={handleSubmit}>Submit</button>
         </div>
       );
     }
